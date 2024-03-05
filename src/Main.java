c631a86d27977ab508690432eeb4f96acc28010d
nicolasdca <nicolasdavid193@gmail.com>
2024-03-05 15:52:27 -0300

    Audio superclass done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
-public class App {
+import br.com.nicolas.alurasound.models.Audio;
+
+public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Audio test = new Audio();
+        test.setTitle("Whom the bells toll");
+        test.setLenght(383);
+        test.setPlaybacks(4);
+        test.setLikes(3);
+
+        test.play();
+        test.like();
     }
 }
---
+++ src/br/com/nicolas/alurasound/models/Audio.java
@@ -0,0 +1,66 @@
+package br.com.nicolas.alurasound.models;
+
+public class Audio {
+    private String title;
+    private int lenght; // default: seconds
+    private int playbacks;
+    private int likes;
+    private double rating;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getLenght() {
+        return lenght;
+    }
+
+    public void setLenght(int lenght) {
+        this.lenght = lenght;
+    }
+
+    public int getPlaybacks() {
+        return playbacks;
+    }
+
+    public void setPlaybacks(int playbacks) {
+        this.playbacks = playbacks;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public void like() {
+        likes++;
+        System.out.println(title + " n. of likes: " + likes + "❤");
+    }
+
+    public void play() {
+        playbacks++;
+        System.out.println("Playing: " + title + "\n" + "00:00 --- " + formatLenght());
+    }
+
+    private String formatLenght() {
+        String formattedLenght;
+        formattedLenght = String.format("%02d:%02d", lenght / 60, lenght % 60);
+        return formattedLenght;
+    }
+
+}
